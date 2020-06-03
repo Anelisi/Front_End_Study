73cbb5a6cd149d9bc3b67040a44497d13aeb408f
Anelisi <anelisilanmeller@gmail.com>
2020-06-03 15:56:52 -0300

    Aula de exercícios do Curso em vídeo

---
+++ aula12exercicicios/exe015/script.js
@@ -0,0 +1,61 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var anof = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (anof.value.lenght == 0 || anof.value > ano) {
+        window.alert('[ERRO] Verifique os dados digitados e tente novamente!')
+    } else {
+        var sexf = document.getElementsByName('radsex')
+        var idade = ano - Number(anof.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (sexf[0].checked) {
+            genero = 'Mulher'
+            if (idade <= 4) {
+                img.setAttribute('src', 'menina_n.png')
+                    // nene
+            } else if (idade < 10) {
+                img.setAttribute('src', 'menina_c.png')
+                    // criança                
+            } else if (idade < 18) {
+                img.setAttribute('src', 'adole_m.png')
+                    // adolescente
+            } else if (idade < 50) {
+                img.setAttribute('src', 'mulher_adulta.png')
+                    // Adulto
+
+            } else {
+                img.setAttribute('src', 'mulher_senhora.png') // Idoso
+
+            }
+        } else if (sexf[1].checked) {
+            genero = 'Homem'
+            if (idade <= 4) {
+                img.setAttribute('src', 'menino_n.png')
+                    // nene
+            } else if (idade < 10) {
+                img.setAttribute('src', 'menino_c.png')
+                    // criança                
+            } else if (idade < 18) {
+                img.setAttribute('src', 'adole_h.png')
+                    // adolescente
+
+            } else if (idade < 50) {
+                img.setAttribute('src', 'homem_adulto.png')
+                    // Adulto
+
+            } else {
+                img.setAttribute('src', 'homem_senhor.png')
+                    // Idoso
+
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos <strong>${genero}</strong> com <strong>${idade}</strong> anos`
+        res.appendChild(img)
+        document.getElementById('botao').style.font = '20px'
+
+    }
+}
